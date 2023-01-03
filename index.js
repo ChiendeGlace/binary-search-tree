a2b12fd76108fa72dea54e89076c12e40b362cf8
ChiendeGlace <szymon.szostak72@gmail.com>
2023-01-03 19:51:59 +0100

    Add preorder, inorder, postorder, balance functions

--- index.js
+++ index.js
@@ -43,7 +43,6 @@ const treeFactory = (array) => {
     root = null;
     const sorted = mergeSort(array);
     const sortedUnique = removeDuplicates(sorted);
-    console.log(sortedUnique);
     const buildTree = (array, start = 0, end) => {
         if (start > end) {
             return null;
@@ -58,71 +57,70 @@ const treeFactory = (array) => {
         return node;
     };
 
-    const insert = (root, data) => {
-        if (root == null) {
-            root = nodeFactory(data);
-            return root;
+    const insert = (data, treeRoot = root) => {
+        if (treeRoot == null) {
+            treeRoot = nodeFactory(data);
+            return treeRoot;
         }
-        if (data > root.data) {
-            root.right = insert(root.right, data);
-        } else if (data < root.data) {
-            root.left = insert(root.left, data);
+        if (data > treeRoot.data) {
+            treeRoot.right = insert(data, treeRoot.right);
+        } else if (data < treeRoot.data) {
+            treeRoot.left = insert(data, treeRoot.left);
         } else {
             return null;
         }
-        return root;
+        return treeRoot;
     };
 
-    const deleteNode = (root, data) => {
-        if (root == null) {
+    const deleteNode = (data, treeRoot = root) => {
+        if (treeRoot == null) {
             return null;
         }
-        if (root.data == data) {
-            if (root.left == null && root.right == null) {
-                root = null;
-                return root;
+        if (treeRoot.data == data) {
+            if (treeRoot.left == null && treeRoot.right == null) {
+                treeRoot = null;
+                return treeRoot;
             }
-            if (root.left == null || root.right == null) {
-                if (root.left == null) {
-                    root = root.right;
-                    return root;
+            if (treeRoot.left == null || treeRoot.right == null) {
+                if (treeRoot.left == null) {
+                    treeRoot = treeRoot.right;
+                    return treeRoot;
                 } else {
-                    root = root.left;
-                    return root;
+                    treeRoot = treeRoot.left;
+                    return treeRoot;
                 }
             }
-            let rightSide = root.right;
+            let rightSide = treeRoot.right;
             while (rightSide.left) {
                 rightSide = rightSide.left;
             }
-            root.data = rightSide.data;
-            root.right = deleteNode(root.right, rightSide.data);
-            return root;
+            treeRoot.data = rightSide.data;
+            treeRoot.right = deleteNode(rightSide.data, treeRoot.right);
+            return treeRoot;
         } else {
-            if (data > root.data) {
-                root.right = deleteNode(root.right, data);
-            } else if (data < root.data) {
-                root.left = deleteNode(root.left, data);
+            if (data > treeRoot.data) {
+                treeRoot.right = deleteNode(data, treeRoot.right);
+            } else if (data < treeRoot.data) {
+                treeRoot.left = deleteNode(data, treeRoot.left);
             }
         }
-        return root;
+        return treeRoot;
     };
 
-    const findNode = (root, data) => {
-        if (root == null) {
+    const findNode = (data, treeRoot = root) => {
+        if (treeRoot == null) {
             return 'Tree is empty or wrong value';
         }
-        if (data == root.data) {
-            return root;
+        if (data == treeRoot.data) {
+            return treeRoot;
         }
-        if (data > root.data) {
-            root.right = findNode(root.right, data);
-            return root.right;
+        if (data > treeRoot.data) {
+            treeRoot.right = findNode(data, treeRoot.right);
+            return treeRoot.right;
         }
-        if (data < root.data) {
-            console.log(root);
-            root.left = findNode(root.left, data);
-            return root.left;
+        if (data < treeRoot.data) {
+            treeRoot.left = findNode(data, treeRoot.left);
+            return treeRoot.left;
         }
     };
 
@@ -130,7 +128,6 @@ const treeFactory = (array) => {
         const arr = [];
         const queue = [];
         queue.push(root);
-        console.log();
         while (queue.length > 0) {
             arr.push(queue[0].data);
             if (queue[0].left !== null) {
@@ -160,7 +157,84 @@ const treeFactory = (array) => {
         return arr;
     };
 
+    const preorder = (treeRoot = root, arr = []) => {
+        if (treeRoot == null) return;
+        arr.push(treeRoot.data);
+        if (treeRoot.left !== null) {
+            preorder(treeRoot.left, arr);
+        }
+        if (treeRoot.right !== null) {
+            preorder(treeRoot.right, arr);
+        }
+        return arr;
+    };
+    const inorder = (treeRoot = root, arr = []) => {
+        if (treeRoot == null) return;
+        if (treeRoot.left !== null) {
+            inorder(treeRoot.left, arr);
+        }
+        arr.push(treeRoot.data);
+        if (treeRoot.right !== null) {
+            inorder(treeRoot.right, arr);
+        }
+        return arr;
+    };
+    const postorder = (treeRoot = root, arr = []) => {
+        if (treeRoot == null) return;
+        if (treeRoot.left !== null) {
+            postorder(treeRoot.left, arr);
+        }
+
+        if (treeRoot.right !== null) {
+            postorder(treeRoot.right, arr);
+        }
+        arr.push(treeRoot.data);
+        return arr;
+    };
+
+    const findHeight = (treeRoot = root) => {
+        if (treeRoot == null) return -1;
+        const leftHeight = findHeight(treeRoot.left);
+        const rightHeight = findHeight(treeRoot.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    };
+
+    const findDepth = (data, treeRoot = root, count = 0) => {
+        if (treeRoot == null) {
+            return 'Tree is empty or wrong value';
+        }
+        if (data == treeRoot.data) {
+            return count;
+        }
+        if (data > treeRoot.data) {
+            return findDepth(data, treeRoot.right, count + 1);
+        }
+        if (data < treeRoot.data) {
+            return findDepth(data, treeRoot.left, count + 1);
+        }
+    };
+    const isBalanced = () => {
+        let leftHeight = findHeight(root.left);
+        let rightHeight = findHeight(root.right);
+        if (Math.abs(leftHeight - rightHeight) <= 1) {
+            return true;
+        }
+        return false;
+    };
+
     root = buildTree(sortedUnique, 0, sortedUnique.length - 1);
+
+    const rebalance = () => {
+        let newArr = inorder();
+        const newSortedArr = mergeSort(newArr);
+        const newSortedUnique = removeDuplicates(newSortedArr);
+        root = buildTree(newSortedUnique, 0, newSortedUnique.length - 1);
+    };
+
+    const display = (treeRoot = root) => {
+        return root;
+    };
+
     return {
         root,
         insert,
@@ -168,6 +242,14 @@ const treeFactory = (array) => {
         findNode,
         levelOrder,
         levelOrderRecursive,
+        preorder,
+        inorder,
+        postorder,
+        findHeight,
+        findDepth,
+        isBalanced,
+        rebalance,
+        display,
     };
 };
 
@@ -175,6 +257,28 @@ const newBinaryTree = treeFactory([
     1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324,
 ]);
 
-newBinaryTree.insert(newBinaryTree.root, 50);
-newBinaryTree.insert(newBinaryTree.root, 320);
-console.log(newBinaryTree.levelOrderRecursive());
+newBinaryTree.insert(50);
+newBinaryTree.insert(320);
+console.log(newBinaryTree.isBalanced());
+console.log(newBinaryTree.levelOrder());
+console.log(newBinaryTree.preorder());
+console.log(newBinaryTree.inorder());
+console.log(newBinaryTree.postorder());
+
+newBinaryTree.insert(659);
+newBinaryTree.insert(3476);
+newBinaryTree.insert(565);
+newBinaryTree.insert(9090);
+newBinaryTree.insert(11092);
+newBinaryTree.insert(201);
+console.log(newBinaryTree.isBalanced());
+console.log(newBinaryTree.display());
+newBinaryTree.rebalance();
+console.log(newBinaryTree.display());
+console.log(newBinaryTree.isBalanced());
+console.log(newBinaryTree.levelOrder());
+console.log(newBinaryTree.preorder());
+console.log(newBinaryTree.inorder());
+console.log(newBinaryTree.postorder());
+console.log(newBinaryTree.findNode(67));
+
